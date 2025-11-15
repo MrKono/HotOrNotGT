01a104b5508e3a4d72b71ae754801a4a1eeddb58
ko_no <90126004+MrKono@users.noreply.github.com>
2025-11-15 21:42:47 +0900

    tiny fix

--- src/main/java/kono/hotornotgt/HotOrNotGT.java
+++ src/main/java/kono/hotornotgt/HotOrNotGT.java
@@ -14,7 +14,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.buuz135.hotornot.HotOrNot;
-import com.cleanroommc.groovyscript.Tags;
 
 import gregtech.GTInternalTags;
 
